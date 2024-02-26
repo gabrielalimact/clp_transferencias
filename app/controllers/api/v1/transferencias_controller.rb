ae012d367cf70458840a703c1cf8b36f51391620
Gabriela <leticia.lima@nees.ufal.br>
2024-02-26 13:01:27 -0300

    adiciona transferencia entre usuarios

---
+++ app/controllers/api/v1/transferencias_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class TransferenciasController < ApplicationController
+      def create
+        id_origem = params[:id_origem]
+        id_destino = params[:id_destino]
+        valor_transferencia = params[:valor].to_f
+
+        usuario_origem = UsuarioPix.find(id_origem)
+        usuario_destino = UsuarioPix.find(id_destino)
+
+        # Verifica se o valor da transferência é maior que zero
+        if valor_transferencia <= 0
+          render json: { error: 'O valor da transferência deve ser maior que zero' }, status: :unprocessable_entity
+          return
+        end
+
+        # Verifica se o valor da transferência é menor ou igual ao saldo do usuário de origem
+        if valor_transferencia > usuario_origem.saldo
+          render json: { error: 'Saldo insuficiente' }, status: :unprocessable_entity
+          return
+        end
+
+        # Realiza a transferência
+        ActiveRecord::Base.transaction do
+          usuario_origem.update(saldo: usuario_origem.saldo - valor_transferencia)
+          usuario_destino.update(saldo: usuario_destino.saldo + valor_transferencia)
+        end
+
+        render json: { message: 'Transferência realizada com sucesso' }, status: :ok
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: 'Usuário não encontrado' }, status: :not_found
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace 'api' do
   	namespace 'v1' do
-  		resources :usuarios_pix
+  		resources :usuarios_pix, only: [:index, :show] do
+        post 'transferir', to: 'transferencias#create'
+
+      end
   	end
   end
 end
